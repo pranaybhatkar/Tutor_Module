16bb984dbec03e72061486306f7e058165e0a0e0
pranaybhatkar <pranaybhatkar21@gmail.com>
2022-04-05 12:18:25 +0530

    First deployment update - twenty-third attempt

--- main.py
+++ main.py
@@ -32,28 +32,6 @@ app.add_middleware(
 )
 
 
-# class Tutor(BaseModel):
-#     username: str
-#     password: str
-
-
-# @app.post("/Add_Tutor")
-# async def add_tutor(tutor_name: str = Form(...), password: str = Form(...)):
-#     print(tutor_name)
-#     list_of_usernames.append(tutor_name)
-#     list_of_passwords.append(password)
-
-# for elements in zip(list_of_usernames, list_of_passwords):
-#     combined_list.append(elements)
-
-# @app.post("/Get-file")
-# async def get_file(data_file: UploadFile = File(...)):
-#     print(data_file.content_type)
-#     contents = await data_file.read()
-#     save_file(data_file.filename, contents)
-#     return {"Filename": data_file.filename}
-
-
 @app.get("/")
 async def index(request: Request, response_class: HTMLResponse):
     return templates.TemplateResponse('input_validation.html', context={"request": request})
@@ -73,25 +51,6 @@ async def get_count():
     # return ["CS_IT_Count", CS_IT_Count], ["Non_CS_IT_Count", Non_CS_IT_Count], ["Total_Count", Total_count]
     return {"count": [CS_IT_Count, Non_CS_IT_Count]}
 
-# @app.get("/CS_IT_Count")
-# def get_cs_it_count():
-#     # return {"The no. of students belonging to CS/IT background is": CS_IT_Count}
-#     return {"CS_IT_Count": CS_IT_Count}
-
-
-# @app.get("/Non_CS_IT_Count")
-# def get_non_cs_it_count():
-#     return {"The no. of students belonging to Non-CS/IT background is ": Non_CS_IT_Count}
-
-
-# @app.get("/Total_Count")
-# def get_total_count():
-#     return {"The total count of students is ": Total_count}
-
-
-# @app.get("/index")
-# def index(request: Request, response_class: HTMLResponse):
-#     return templates.TemplateResponse('welcome.html', context={"request": request})
 
 @app.get("/Tech_Stack_Certifications")
 async def get_tech_stack_certifications():
