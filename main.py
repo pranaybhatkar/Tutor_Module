a0175b504f31de5020ae13fefe027b3906d0afe7
pranaybhatkar <pranaybhatkar21@gmail.com>
2022-06-20 19:33:38 +0530

    output segragation-attempt_14

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Path, Request, File, UploadFile, Form
-from typing import Optional
+from fastapi import FastAPI, Path, Request, File, UploadFile, Form, Depends, HTTPException, status
+from typing import Optional, Union
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import uvicorn
 
 from Priorities import CS_IT_Count, Non_CS_IT_Count, Total_count, unique_list, final_answer_as_tuple, \
@@ -12,6 +13,7 @@ from Priorities import CS_IT_Count, Non_CS_IT_Count, Total_count, unique_list, f
 app = FastAPI()  # an instance of the imported FastAPI. Through this instance, we can use multiple
 # methods associated with FastAPI.
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -30,18 +32,93 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+users_db = {
+    "pranay": {
+        "username": "pranay",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "manav": {
+        "username": "manav",
+        "hashed_password": "fakehashedsecret_2",
+        "disabled": True,
+    },
+}
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+class User(BaseModel):
+    username: str
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/input_details")
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return templates.TemplateResponse('welcome.html', context={"request": request})
+    # return {"access_token": user.username, "token_type": "bearer"}
+
+
+# @app.get("/users/me")
+# async def read_users_me(current_user: User = Depends(get_current_active_user)):
+#     return current_user
+
 
 @app.get("/")
 async def index(request: Request, response_class: HTMLResponse):
     return templates.TemplateResponse('input_validation.html', context={"request": request})
 
 
-@app.post("/input_details")
-def index(request: Request, tutor_name: str = Form(...), password: str = Form(...)):
-    if tutor_name == 'admin' and password == 'password':
-        return templates.TemplateResponse('welcome.html', context={"request": request})
-    else:
-        return {"Access Denied": "Enter Admin as user-name and 'password' as password"}
+# @app.post("/input_details")
+# def index(request: Request, tutor_name: str = Form(...), password: str = Form(...)):
+#     if tutor_name == 'admin' and password == 'password':
+#         return templates.TemplateResponse('welcome.html', context={"request": request})
+#     else:
+#         return {"Access Denied": "Enter Admin as user-name and 'password' as password"}
 
 
 @app.get("/output")
@@ -52,6 +129,7 @@ async def index(request: Request, response_class: HTMLResponse):
 @app.get("/previous_output")
 async def index(request: Request, response_class: HTMLResponse):
     return templates.TemplateResponse('welcome.html', context={"request": request})
+
 
 @app.get("/Count")
 async def get_count():
